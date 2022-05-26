3c800437467bcb3a3cebfef5a8028b5fe9022a92
Wesley Schreur <info@wesleyschreur.nl>
2022-05-26 16:09:45 +0200

    Added 2 vars and includes from URL

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 	$db_info;
+	$link;
+	$data;
 
 	include_once('resources/php/settings.php');
 
@@ -15,7 +17,6 @@
 		$db_info['message'] = 'Database connection is working.';
 
 		//TODO include functions
-		echo 1;
 
 	} catch (mysqli_sql_exception $error) {
 
@@ -25,14 +26,58 @@
 		$db_info['error_msg'] = mysqli_connect_error();
 
 		//TODO show maintenance page
-		echo 2;
 
 	}
 
 	// Run everything else when the database connection is working
 	if ($db_info['status'] == 'OK') {
+	
+		if (!isset($_GET['data']) || empty($_GET['data'])) {
 
-		// ...
+			$_GET['data'] = 'empty';
+			$data[0] = 'empty';
+
+		} else {
+
+			$data = explode('/', $_GET['data']);
+
+			if (empty($data[1])) {
+
+				unset($data[1]);
+
+			}
+
+		}
+
+		if ($data[0] == 'empty') {
+
+			include_once ('pages/includes/_header.php');
+			include_once ('pages/main.php');
+			include_once ('pages/includes/_footer.php');
+
+		} else if ($data[0] == 'admin') {
+
+			include_once ('pages/admin/admin.php');
+
+		} else {
+
+			if (file_exists('pages/' . $data[0] . '.php')) {
+
+				include_once ('pages/includes/_header.php');
+				include_once ('pages/' . $data[0] . '.php');
+				include_once ('pages/includes/_footer.php');
+
+			} else {
+
+				$data[0] = '404';
+
+				include_once ('pages/includes/_header.php');
+				include_once ('pages/404.php');
+				include_once ('pages/includes/_footer.php');
+
+			}
+
+		}
 
 	}
 
