5c3eba937773c306f3563a3b9d80591d64056f26
Wesley Schreur <info@wesleyschreur.nl>
2022-05-26 18:45:14 +0200

    Default pages for the admin section

---
+++ pages/admin/includes/_header.php
@@ -0,0 +1,10 @@
+header<br><br>
+
+<!-- Navigation -->
+<nav>
+	<ul>
+		<li><a href="/admin">Home</a></li>
+		<li><a href="/admin/dashboard">Dashboard</a></li>
+		<li><a href="/admin/settings">Settings</a></li>
+	</ul>
+</nav>
